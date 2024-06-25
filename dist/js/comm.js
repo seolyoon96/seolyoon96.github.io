901c8451f11453ff8230615ca09af1cba1d79c41
seolyoon96 <tjf118@naver.com>
2024-06-25 11:07:19 +0900

    PORTFOLIO 수정

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -4,3 +4,19 @@ document.querySelector("#contact-form").addEventListener("submit", (e) => {
   e.target.elements.email.value = "";
   e.target.elements.message.value = "";
 });
+
+document.body.addEventListener('mousemove', (evt) => {
+  const mouseX = evt.clientX;
+  const mouseY = evt.clientY;
+
+  gsap.set('.cursor', {
+    x: mouseX,
+    y: mouseY,
+  });
+
+  gsap.to('.shape', {
+    x: mouseX,
+    y: mouseY,
+    stagger: -0.1,
+  });
+});
